ca4179745e87a04ac2e1994d9ca5ca2fc6e0e9c7
Shreykhanna <khannashrey07@gmail.com>
2025-08-14 17:55:09 +1000

    Fix get arnie quotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,21 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const successKey = "Arnie Quote",
+    failureKey = "FAILURE";
+
+  const results = await Promise.all(
+    urls.map(async (url) => {
+      const { status, body } = await httpGet(url);
+      const { message } = JSON.parse(body);
+
+      return status === 200
+        ? { [successKey]: message }
+        : { [failureKey]: message };
+    })
+  );
+
+  return results;
 };
 
 module.exports = {
